8e6c7a0e1732b0dee9d5b215bf67d80fd1bca7a9
bxq2011hust <bxq2011hust@qq.com>
2021-05-10 17:58:21 +0800

    add install command

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,8 +1,8 @@
 
 # hunter_config(Boost VERSION 1.76.0)
 hunter_config(bcos-framework VERSION 3.0.0-5e937164
-    URL https://github.com/FISCO-BCOS/bcos-framework/archive/5e937164b78a80a8d81397bbaf9f28d6488b874d.tar.gz
-    SHA1 0526295ca6313c87bba0c96d85284540c37e08bb
+    URL https://github.com/FISCO-BCOS/bcos-framework/archive/e88edc774df5f2d44e0f042992328581197a9085.tar.gz
+    SHA1 65be827a5e8d82e5adb05a7e5c3b8701166dd552
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
