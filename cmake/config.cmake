20acdcd83c0972f50acf97b2db7188487bb8e09d
morebtcg <651932351@qq.com>
2021-05-14 09:29:30 +0800

    replace github.com with ${URL_BASE}

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,13 +1,13 @@
 
 # hunter_config(Boost VERSION 1.76.0)
 hunter_config(bcos-framework VERSION 3.0.0-5e937164
-    URL https://github.com/FISCO-BCOS/bcos-framework/archive/232ee40983b3df32fcedf4477b50c55941154659.tar.gz
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/232ee40983b3df32fcedf4477b50c55941154659.tar.gz
     SHA1 32fdb93d905ac42307276f12451bca65a2ae3c35
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
 hunter_config(rocksdb VERSION 6.19.3
-    URL https://github.com/facebook/rocksdb/archive/refs/tags/v6.19.3.tar.gz
+	URL https://${URL_BASE}/facebook/rocksdb/archive/refs/tags/v6.19.3.tar.gz
     SHA1 27c18025d739e83fe3819e48f2f4dfcc43526462
     CMAKE_ARGS WITH_TESTS=OFF
     WITH_GFLAGS=OFF
