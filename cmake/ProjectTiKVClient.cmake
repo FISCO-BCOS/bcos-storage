61fd3be9dec5df2828cb42155a863e54c9c9f41e
bxq2011hust <bxq2011hust@qq.com>
2021-10-09 10:48:06 +0800

    update deps of gRPC

--- cmake/ProjectTiKVClient.cmake
+++ cmake/ProjectTiKVClient.cmake
@@ -6,10 +6,13 @@ hunter_add_package(Protobuf)
 find_package(Protobuf CONFIG REQUIRED)
 hunter_add_package(gRPC)
 find_package(gRPC CONFIG REQUIRED)
+hunter_add_package(PocoCpp)
+find_package (Poco REQUIRED Foundation Net JSON Util)
+
 set(ENV{PATH} ${GRPC_ROOT}/bin:$ENV{PATH})
 FetchContent_Declare(tikv_client_project
   GIT_REPOSITORY https://${URL_BASE}/bxq2011hust/client-c.git
-  GIT_TAG        41913220562be1af13e160d5a72dc175ba62cfd9
+  GIT_TAG        2a3fca44cefe6d67ff19ded6171e66ade0d4f1c4
   # SOURCE_DIR     ${CMAKE_SOURCE_DIR}/deps/src/
   PATCH_COMMAND  export PATH=${GRPC_ROOT}/bin:\$PATH COMMAND protoc --version COMMAND bash third_party/kvproto/scripts/generate_cpp.sh
   # LOG_BUILD true
@@ -28,4 +31,4 @@ endif()
 
 add_library(bcos::tikv_client INTERFACE IMPORTED)
 set_property(TARGET bcos::tikv_client PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${tikv_client_project_SOURCE_DIR}/include ${GRPC_ROOT}/include)
-set_property(TARGET bcos::tikv_client PROPERTY INTERFACE_LINK_LIBRARIES kv_client gRPC::grpc++)
+set_property(TARGET bcos::tikv_client PROPERTY INTERFACE_LINK_LIBRARIES kv_client gRPC::grpc++ Poco::Foundation)
